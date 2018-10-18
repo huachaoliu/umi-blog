294a0057f8a8a3b36bbfeebe84e938c585824bc3
刘华超 <huachao.liu@daxiangyun.com>
2018-10-18 18:04:24 +0800

    style:调整央视

--- src/pages/type/index.js
+++ src/pages/type/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import router from 'umi/router';
 import { connect } from 'dva';
 import css from './index.less';
 
 @connect(state => state.type)
 export default class Types extends React.PureComponent {
-
+  _handlerRouteChange = (key) => {
+    router.push(`/type/${key}`);
+  }
   render() {
     const { list } = this.props;
     console.log(list);
@@ -12,8 +15,7 @@ export default class Types extends React.PureComponent {
       <div className={css.type}>
         {
           list.map((tag, i) => {
-
-            return <div key={i} className={css.tag}>
+            return <div key={i} className={css.tag} onClick={this._handlerRouteChange.bind(this, tag.key)}>
               <span>{tag.key}</span>
               <div className={css.mask} style={{ background: tag.color }}>{tag.key}</div>
             </div>
